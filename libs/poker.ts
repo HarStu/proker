f117564c467c8e06f58ebe273f07ca533280b8c7
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-30 17:03:21 -0400

    import functions from headsup project

---
+++ libs/poker.ts
@@ -0,0 +1,38 @@
+// Four suits, hearts diamonds clubs spades
+export type Suit = 'h' | 'd' | 'c' | 's'
+// ranks from 2-14, with 11 being jack, 12 being queen, 13 being king, and 14 being ace
+export type Rank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14
+
+// each card has a suit and a rank
+export type Card = {
+  rank: Rank;
+  suit: Suit;
+}
+
+export type Deck = Card[]
+
+// return a shuffled Deck
+function generateDeck(): Deck {
+  const validSuits: Suit[] = ['h', 'd', 'c', 's']
+  const validRanks: Rank[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+  // Create an unshuffledDeck
+  let unshuffledDeck: Deck = []
+  for (let cardSuit of validSuits) {
+    for (let cardRank of validRanks) {
+      unshuffledDeck.push({ suit: cardSuit, rank: cardRank })
+    }
+  }
+
+  // Shuffle the deck
+  let shuffledDeck: Deck = []
+  while (unshuffledDeck.length > 0) {
+    // splice(index, n) removes n elements starting at index, and returns them as an array
+    // so here we're grabbing 1 card from a random location in unshuffledDeck, 
+    // then grabbing it from the array it's returned in
+    const cardIndex = Math.floor(Math.random() * (unshuffledDeck.length));
+    shuffledDeck.push(unshuffledDeck.splice(cardIndex, 1)[0]);
+  }
+
+  return shuffledDeck
+}
